3370f751190a8ce2e8cf6e6c55bf210cfc43fb42
Ivan Storck <ivanoats@gmail.com>
2015-09-24 12:36:51 -0700

    trying vendor

--- brunch-config.js
+++ brunch-config.js
@@ -13,7 +13,7 @@ module.exports = {
     plugins: {
       elmBrunch: {
         mainModules: ["src/Bingo.elm", "src/BingoUtils.elm"],
-        outputFolder: "public"
+        outputFolder: "vendor"
       }
     }
   }
