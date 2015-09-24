fe7cf93b91289ce8f33a936ccb0e4bae2c7accab
Ivan Storck <ivanoats@gmail.com>
2015-09-24 16:06:25 -0700

    lesson 9 done

--- src/Bingo.elm
+++ src/Bingo.elm
@@ -5,6 +5,14 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (toUpper, repeat, trimRight)
 
+newEntry phrase points id =
+  { phrase = phrase,
+    points = points,
+    wasSpoken = False,
+    id = id
+  }
+
+
 title message times =
   message ++ " "
     |> toUpper
@@ -16,23 +24,26 @@ title message times =
 pageHeader =
   h1 [ ] [title "bingo!" 3]
 
+
 pageFooter =
   footer [ ]
     [ a [ href "https://ivanstorck.com"]
         [ text "by Ivan Storck" ] ]
 
-entryItem phrase points =
+
+entryItem entry =
   li [ ]
-   [ span [ class "phrase" ] [ text phrase ],
-     span [ class "points" ] [ text (toString points) ]
+   [ span [ class "phrase" ] [ text entry.phrase ],
+     span [ class "points" ] [ text (toString entry.points) ]
    ]
 
 
 entryList =
   ul [ ]
-    [ entryItem "Future-Proof" 100,
-      entryItem "Doing Agile" 200
+    [ entryItem (newEntry "Future-Proof" 100 1),
+      entryItem (newEntry "Doing Agile" 200 2)
     ]
+
 
 view =
   div [ id "container" ]
